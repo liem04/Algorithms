f9551b21ab7606ac0792be2f855f7a06bbc0709c
Bui Thanh Liem <liembt@coccoc.vn>
2017-05-23 18:50:50 +0700

    update kmin

--- app/kmin.php
+++ app/kmin.php
@@ -11,39 +11,16 @@ require_once __DIR__ . '/../vendor/autoload.php';
 $fileReader = new FileReader();
 list($a, $k) = $fileReader->read(__DIR__ . '/input.txt');
 
-//Using MinHeap
-$timeStart = microtime(true);
-$usingMinHeap = new UsingMinHeap();
-$kmin = $usingMinHeap->solve($a, $k);
-$timeEnd = microtime(true);
-echo 'Using MinHeap:            ';
-dispayArray($kmin, $timeEnd - $timeStart);
-
-//Using HeapSort
-$timeStart = microtime(true);
-$usingHeapSort = new UsingHeapSort();
-$kmin = $usingHeapSort->solve($a, $k);
-$timeEnd = microtime(true);
-echo 'Using HeapSort:           ';
-dispayArray($kmin, $timeEnd - $timeStart);
-
-//Using RandomizedSelect
-$timeStart = microtime(true);
-$usingRandomizedSelect = new UsingRandomizedSelect();
-$kmin = $usingRandomizedSelect->solve($a, $k);
-$timeEnd = microtime(true);
-echo 'Using RandomizedSelect:   ';
-dispayArray($kmin, $timeEnd - $timeStart);
-
-//Using Select
-$timeStart = microtime(true);
-$usingSelect = new UsingSelect();
-$kmin = $usingSelect->solve($a, $k);
-$timeEnd = microtime(true);
-echo 'Using Select:             ';
-dispayArray($kmin, $timeEnd - $timeStart);
-
-function dispayArray(array $a, $time)
-{
-    echo implode(' ', $a) . ' ' . $time . PHP_EOL;
+$methods = [
+    'MinHeap         ' => new UsingMinHeap(),
+    'HeapSort        ' => new UsingHeapSort(),
+    'RandomizedSelect' => new UsingRandomizedSelect(),
+    'Select          ' => new UsingSelect()
+];
+foreach ($methods as $name => $method) {
+    //Using MinHeap
+    $timeStart = microtime(true);
+    $kmin = $method->solve($a, $k);
+    $timeEnd = microtime(true);
+    echo $name . ': ' . implode(' ', $kmin) . ' ' . ($timeEnd - $timeStart) . PHP_EOL;
 }
